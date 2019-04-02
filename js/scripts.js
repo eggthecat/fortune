bd0021a581870bf58f80900e87c43ce0375fc00c
dominic montelongo and jaime takikawa <dominic@>
2019-04-02 15:26:01 -0700

    Add proper syntax thanks to Nathan

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,18 @@
 $(document).ready(function() {
-  $("#form").submit(function(event) {
-  };
+  $("#fortunequiz").submit(function(event) {
+    event.preventDefault();
+    //
+    var points = 0;
+    $("input:checkbox[name=For]:checked").each(function(){
+      points++;
+    });
+    $(".results").hide();
+    if(points <= 3){
+      $(".Ans1").show();
+    } else if (points <= 6){
+      $(".Ans2").show();
+    } else if (points > 6){
+      $(".Ans3").show();
+    }
   });
 });
